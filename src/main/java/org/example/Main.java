c3d0dae0a7c069dc1aff58aae9068ffb2e3f2c5d
Maruoka <dm.hiro@gmail.com>
2023-02-05 23:40:20 +0900

    初期コミット

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import java.util.List;
+
+public class Main {
+    static String bucketName = "tekito-na-bucket-name";
+    public static void main(String[] args) {
+        System.out.println("Hello s3!");
+        Region region = Region.AP_NORTHEAST_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+        listBucketObjects(s3, bucketName);
+    }
+    public static void listBucketObjects(S3Client s3, String bucketName ) {
+
+        try {
+            ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .build();
+
+            ListObjectsResponse res = s3.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            for (S3Object myValue : objects) {
+                System.out.print("\n The name of the key is " + myValue.key());
+                System.out.print("\n The object is " + calKb(myValue.size()) + " KBs");
+                System.out.print("\n The owner is " + myValue.owner());
+            }
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+
+    //convert bytes to kbs.
+    private static long calKb(Long val) {
+        return val/1024;
+    }
+}
