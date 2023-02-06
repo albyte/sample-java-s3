428525dac3362b4eef9e4bc07c279beeb46b09d0
Maruoka <dm.hiro@gmail.com>
2023-02-06 17:40:46 +0900

    S3Asyncを利用するよう修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,63 @@
 package org.example;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.net.URI;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
-    static String bucketName = "tekito-na-bucket-name";
+
     public static void main(String[] args) {
         System.out.println("Hello s3!");
-        Region region = Region.AP_NORTHEAST_1;
-        S3Client s3 = S3Client.builder()
-                .region(region)
-                .build();
-        listBucketObjects(s3, bucketName);
+        ResourceBundle rb = ResourceBundle.getBundle("app");
+
+        listBucketObjects(
+                rb.getString("accessKey"),
+                rb.getString("secretKey"),
+                rb.getString("endPointUrl"),
+                rb.getString("bucketName")
+        );
     }
-    public static void listBucketObjects(S3Client s3, String bucketName ) {
+
+    public static void listBucketObjects(String accessKey, String secretKey, String endPointUrl, String bucketName) {
 
         try {
+            AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+            S3AsyncClient s3AsyncClient = S3AsyncClient.crtBuilder()
+                    .region(Region.AP_NORTHEAST_1)
+                    .endpointOverride(new URI(endPointUrl))
+                    .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                    .build();
+
             ListObjectsRequest listObjects = ListObjectsRequest
                     .builder()
                     .bucket(bucketName)
                     .build();
 
-            ListObjectsResponse res = s3.listObjects(listObjects);
-            List<S3Object> objects = res.contents();
+            CompletableFuture<ListObjectsResponse> res = s3AsyncClient.listObjects(listObjects);
+            List<S3Object> objects = res.get().contents();
+
+            System.out.print("\n");
             for (S3Object myValue : objects) {
-                System.out.print("\n The name of the key is " + myValue.key());
-                System.out.print("\n The object is " + calKb(myValue.size()) + " KBs");
-                System.out.print("\n The owner is " + myValue.owner());
-                System.out.print("\n");
+                System.out.print(myValue.key() + " (" + calKb(myValue.size()) + " KBs)\n");
             }
 
-        } catch (S3Exception e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
             System.exit(1);
         }
     }
 
     //convert bytes to kbs.
     private static long calKb(Long val) {
-        return val/1024;
+        return val / 1024;
     }
 }
